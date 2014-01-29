f72733bf38d060d0219a933b3e867047776a017e
Daniel Hahler <git@thequod.de>
2014-01-29 15:05:21 +0100

    autopep8: fix E122

--- src/test_project/test_project/settings.py
+++ src/test_project/test_project/settings.py
@@ -79,7 +79,7 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # Make this unique, and don't share it with anybody.
@@ -89,7 +89,7 @@ SECRET_KEY = '-0xxc*v@#j1kax(y=7+a^)32pa=a%y=ck6lolw=t5j2%h%sl(j'
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+    #     'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
